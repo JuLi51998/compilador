8c6c44535f0bc48a9cb85a70a15b4a218099865d
Julian Goemz <gomezjulian51998@gmail.com>
2019-11-27 13:40:51 -0500

    analisi lexico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'compilador';
+  textoIn: FormGroup;
+  textoLexico: any;
+  textoLineas: Array<string> = [];
+  
+  palabrasEnLenguaje: any = [];
+  // palabrasFueraLenguaje: any = [];
+
+  lenguaje:any = ["0","1","2","3","4","5","6","7","8","9","+","*","-","/","%",";","}","{","if","","do","while","for","=","var","(",")"];
+
+  createForm() {
+    return new FormGroup({
+      palabra: new FormControl('', [Validators.required]),
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.textoIn = this.createForm();
+  }
+
+  
+
+  lexico(texto) {
+    this.textoLexico = texto.palabra.split("\n");
+    
+    for(let i = 0; i < this.textoLexico.length; i++) {
+      this.textoLineas.push(this.textoLexico[i].split(" "));
+    }
+    
+    this.textoLineas.forEach(element => {
+      element.forEach(elemento => {
+        for(let i = 0; i <= this.lenguaje.length; i++){
+          if (this.lenguaje[i] == elemento) {
+            this.palabrasEnLenguaje.push(elemento);
+          }
+        }
+      });
+    });  
+    //console.log("Palabras en lenguaje:", this.palabrasEnLenguaje);
+    this.sintactico(this.palabrasEnLenguaje);
+  }
+
+  sintactico(texto) {
+    console.log("estas en metodo 2", texto);
+  }
+
+  semantico() {
+
+  }
+
+  validate() {
+    if(this.textoIn.valid) {
+      //console.log(this.textoIn.value);
+      this.lexico(this.textoIn.value);
+    } else {
+      console.log("puto");
+    }
+    
+  }
+
 }
